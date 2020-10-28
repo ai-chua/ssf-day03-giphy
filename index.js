b0d7a3e677fbdf3cd080c5418c99647fe519c186
Ailin Chua <ai-chua@outlook.com>
2020-10-28 23:50:50 +0800

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fetch = require('node-fetch')
 const withQuery = require('with-query').default
 
 // Configure the environment
-const PORT = parseInt(process.env.APP_PORT) || 3000
+const PORT = parseInt(process.env.PORT) || 3000
 
 // Create an instance of the express application
 const app = express()
